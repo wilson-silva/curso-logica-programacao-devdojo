0e35897bb90f03d685215e831eaed3ae08e23401
wngv <wilson.silva@gft.com>
2022-11-30 12:14:46 -0300

    criando repositorio para curso de logica

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+M
+src/CalculatorDifference.java,5\f\5f7846f50540167bead5ae35f0bd7250af66f480
---
+++ src/CalculatorDifference.java
@@ -0,0 +1,8 @@
+public class CalculatorDifference{
+    public static void main(String[] args) {
+        int firstAge = 30;
+        int secondAge = 60;
+        int ageDifference = firstAge - secondAge;
+        System.out.println("A diferença entre idade eh: " + ageDifference);
+    }
+}
